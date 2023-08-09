6bfe61e8c56a1bf163c28574721e97f3f74fdc91
marica <bottegamarica02@gmail.com>
2023-08-09 17:09:02 +0200

    creazione main scene non va css

--- FlyPass/src/main/java/com/example/lez5/ControllerMainScene.java
+++ FlyPass/src/main/java/com/example/lez5/ControllerMainScene.java
@@ -1,4 +1,108 @@
 package com.example.lez5;
 
-public class ControllerMainScene {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ControllerMainScene implements Initializable {
+    @FXML
+    private GridPane serviceGrid;
+
+    private List<Service> services;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        services = new ArrayList<>(getServices());
+        int column = 0;
+        int row = 1;
+        try {
+            for (Service service : services) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("service.fxml"));
+                Pane pane = fxmlLoader.load();
+                ControllerService controllerService = fxmlLoader.getController();
+                controllerService.setData(service);
+
+                if(column == 3){
+                    column = 0;
+                    ++row;
+                }
+                serviceGrid.add(pane, column++, row);
+                GridPane.setMargin(pane, new Insets(20));
+            }
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private List<Service> getServices(){
+        List<Service> ls = new ArrayList<Service>();
+        //Rilascio prima volta
+        Service service = new Service();
+        service.setName("Rilascio prima volta");
+        service.setDescription("Rilascio del passaporto per la prima volta");
+        service.setImgSrc("/img/firstTime.jpg");
+        ls.add(service);
+
+        //Rilascio scadenza
+        service = new Service();
+        service.setName("Rinnovo per scadenza");
+        service.setDescription("Rilascio del passaporto per scadenza");
+        service.setImgSrc("/img/scadenza.jpg");
+        ls.add(service);
+
+        //Furto o smarrimento
+        service = new Service();
+        service.setName("Furto o smarrimento");
+        service.setDescription("Rilascio del passaporto per furto o smarrimento");
+        service.setImgSrc("/img/furto.jpg");
+        ls.add(service);
+
+        //Rilascio detoriamento
+        service = new Service();
+        service.setName("Rinnovo per detoriamento");
+        service.setDescription("Rilascio del passaporto per detoriamento");
+        service.setImgSrc("/img/detoriamento.jpg");
+        ls.add(service);
+
+        //passaporto urgente
+        service = new Service();
+        service.setName("Passaporto urgente");
+        service.setDescription("Rilascio del passaporto urgentemente");
+        service.setImgSrc("/img/urgente.jpg");
+        ls.add(service);
+
+        //prolungamento validità passsaporto
+        service = new Service();
+        service.setName("Prolungamento validità passaporto");
+        service.setDescription("prolungamento della validità del passaporto");
+        service.setImgSrc("/img/scadenza.jpg");
+        ls.add(service);
+
+        //cambio info personali
+        service = new Service();
+        service.setName("Cambio info personali");
+        service.setDescription("Iter per il cambio delle informazioni personali");
+        service.setImgSrc("/img/cambio.jpg");
+        ls.add(service);
+
+        //passaporto per minori
+        service = new Service();
+        service.setName("Passaporto per minori");
+        service.setDescription("Rilascio passaporto per minori");
+        service.setImgSrc("/img/minori.jpg");
+        ls.add(service);
+
+
+
+        return ls;
+    }
 }
--- FlyPass/src/main/java/com/example/lez5/ControllerService.java
+++ FlyPass/src/main/java/com/example/lez5/ControllerService.java
@@ -1,4 +1,24 @@
 package com.example.lez5;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ControllerService {
+    @FXML
+    private Label description;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label name;
+    public void setData(Service service){
+        Image image = new Image(getClass().getResourceAsStream(service.getImgSrc()));
+        img.setImage(image);
+        name.setText(service.getName());
+        description.setText(service.getDescription());
+    }
+
 }
