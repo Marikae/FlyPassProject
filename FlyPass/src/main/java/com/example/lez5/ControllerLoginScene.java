0028b67cca1f731fbcd02cb4572b1d3476569b73
marica <bottegamarica02@gmail.com>
2023-10-28 15:47:02 +0200

    sistemata generazione della schermata principale

--- FlyPass/src/main/java/com/example/lez5/ControllerLoginScene.java
+++ FlyPass/src/main/java/com/example/lez5/ControllerLoginScene.java
@@ -9,7 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.io.IOException;
 import java.util.Objects;
 
@@ -50,7 +53,15 @@ public class ControllerLoginScene {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainScene.fxml")));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+       // stage.initStyle(StageStyle.TRANSPARENT); // Rimuovi i bordi della finestra
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+        double windowWidth = screenWidth + 30; // Larghezza della finestra
+        double windowHeight = screenHeight + 1; // Altezza della finestra
+        stage.setX((screenWidth - windowWidth) / 2);
+        stage.setY((screenHeight - windowHeight) / 2);
         stage.setScene(scene);
+        //stage.setFullScreen(true);
         stage.show();
     }
 }
--- FlyPass/src/main/java/com/example/lez5/ControllerRegisterScene.java
+++ FlyPass/src/main/java/com/example/lez5/ControllerRegisterScene.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -111,7 +112,13 @@ public class ControllerRegisterScene {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainScene.fxml")));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
-            stage.setScene(scene);
+            // stage.initStyle(StageStyle.TRANSPARENT); // Rimuovi i bordi della finestra
+            double screenWidth = Screen.getPrimary().getBounds().getWidth();
+            double screenHeight = Screen.getPrimary().getBounds().getHeight();
+            double windowWidth = screenWidth + 30; // Larghezza della finestra
+            double windowHeight = screenHeight + 1; // Altezza della finestra
+            stage.setX((screenWidth - windowWidth) / 2);
+            stage.setY((screenHeight - windowHeight) / 2);            stage.setScene(scene);
             stage.show();
         }
 
